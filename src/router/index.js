6a91ebe15427d29ffe39107c4f116fd5b99c50bd
leo <leohan@hfjy.com>
2017-04-07 10:52:30 +0800

    更新代码

--- src/router/index.js
+++ src/router/index.js
@@ -3,14 +3,34 @@ import Router from 'vue-router'
 // import Login from '@/components/Login'
 import Index from '@/components/Index'
 import MyNews from '@/components/user/MyNews'
-
+import NewContent from '@/components/user/NewContent'
+import First from '@/components/First'
+import NewsDone from '@/components/user/NewsDone'
 Vue.use(Router)
 const router = new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
-      name: 'Index',
-      component: Index
+      component: Index,
+      children: [
+        {
+          path: '/',
+          component: First
+        },
+        {
+          path: '/user/sendnews',
+          component: NewsDone
+        },
+        {
+          path: '/user/mynews',
+          component: MyNews
+        },
+        {
+          path: '/newsinfo',
+          component: NewContent
+        }
+      ]
     },
     {
       path: '/user/mynews',
